302c56bea9ea54fe557d20bdde99098eebabe0db
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-03 13:41:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -17,80 +17,74 @@ if (req.url == "/login.html")
 		res.write(txt);
 	});
 }
-else if(req.url == "/process"){
-		res.writeHead(200, {'Content-Type':'text/html'})
-		console.log("Process the form");
-		pdata = "";
-		req.on('data', data => {
-			   pdata += data.toString();
-		});
-		req.on('end', () => {
-		pdata = qs.parse(pdata);
-			var name = String(pdata['fullname']);
-			res.write("name: ");
-			res.write(name);
-			var Email = String(pdata['email']);
-			res.write(" email: ");
-			res.write(Email);
-			MongoClient.connect(urll, { useUnifiedTopology: true }, function(err, db) {
-			  if(err) { return console.log(err); }
-
-				var dbo = db.db("users");
-				var collection = dbo.collection('profiles');
-				var theQuery = {email: Email} 
-					collection.find(theQuery).toArray(function(err, items) {
-						  if (err) {
-							console.log("Error: '" + err+"'}");
-						  } 
-						  else if(items.length == 0){
-							  var newData = {"fullname": name, "email": Email,"foods":[]};
-							  collection.insertOne(newData, function(err, res){
-								  if(err) { 
-									  console.log("query err: " + err); 
-									  return; 
-								}
-							  console.log("new document inserted");
-						});
-					} 
-
-					});
-
-				setTimeout(function(){db.close;}, 2000);
-				console.log("Success!");
-
-			});  
-		});
-
-	}
 if(req.url == "/my_choice.html"){
-		file2 = 'my_choice.html';
-		fs.readFile(file2, function(err, txt) {
-			if(err) {return console.log(err); }
-			res.write(txt);
-		});
-
+	file2 = 'my_choice.html';
+	fs.readFile(file2, function(err, txt) {
+		if(err) {return console.log(err); }
+		res.write(txt);
+	});
 }
-else if (req.url == "/process"){
-			res.writeHead(200, {'Content-Type':'text/html'})
-			console.log("Process the form");
-			pdata = "";
-			req.on('data', data => {
-				   pdata += data.toString();
-			});
-			req.on('end', () => {
-				pdata = qs.parse(pdata);
-				MongoClient.connect(url2,{useUnifiedTopology:true},function(err, db) {
-					if (err) {
-						return console.log("err");
-					}
-					var dbo = db.db("tuftsdining");
-					var coll = dbo.collection("menu");
+else 
+{
+	res.writeHead(200, {'Content-Type':'text/html'});
+	console.log("Process the form");
+	pdata = "";
+	req.on('data', data => {
+           pdata += data.toString();
+    });
+	req.on('end', () => {
+	pdata = qs.parse(pdata);
+	if (req.url == "login.html/process"){
+		var name = String(pdata['fullname']);
+		res.write("name: ");
+		res.write(name);
+		var Email = String(pdata['email']);
+		res.write(" email: ");
+		res.write(Email);
+		MongoClient.connect(urll, { useUnifiedTopology: true }, function(err, db) {
+		  if(err) { return console.log(err); }
 
-					getFood(pdata['foodname'],coll);
+			var dbo = db.db("users");
+			var collection = dbo.collection('profiles');
+			var theQuery = {email: Email} 
+				collection.find(theQuery).toArray(function(err, items) {
+					  if (err) {
+						console.log("Error: '" + err+"'}");
+					  } 
+					  else if(items.length == 0){
+						  var newData = {"fullname": name, "email": Email,"foods":[]};
+						  collection.insertOne(newData, function(err, res){
+							  if(err) { 
+								  console.log("query err: " + err); 
+								  return; 
+							}
+						  console.log("new document inserted");
+					});
+				} 
 
-					setTimeout(function(){ db.close(); console.log("Success!");}, 2000);
 				});
-			});
+
+			setTimeout(function(){db.close;}, 2000);
+			console.log("Success!");
+
+		});  
+	}
+		if (req.url == "my_choice.html/process"){
+		MongoClient.connect(url2,{useUnifiedTopology:true},function(err, db) {
+			if (err) {
+				return console.log("err");
+			}
+			var dbo = db.db("tuftsdining");
+			var coll = dbo.collection("menu");
+
+			getFood(pdata['foodname'],coll);
+
+			setTimeout(function(){ db.close(); console.log("Success!");}, 2000);
+		});
+		}
+	});
+
+
 }
 setTimeout(function(){res.end();}, 3000);
 }).listen(port);

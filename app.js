52a4c30db577fade1ce73e3795d82c8b135f0ea5
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-03 15:47:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var express = require('express');
-var app = express();
 var fs = require('fs');
 var qs = require('querystring');
 var port = process.env.PORT || 3000;
 const { MongoClient } = require("mongodb");
 const urll = process.env.MONGODB_URLL;
 const url2 = "mongodb+srv://annalisejacobson:annalise@cluster0.0y4mi.mongodb.net/tuftsdining?retryWrites=true&w=majority";
+var app = express();
 app.use(express.static("public"));
 
 

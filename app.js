4727f426a7f3bc45ceaba8e9ca62232496efdf3e
unnathynellutla <78392199+unnathynellutla@users.noreply.github.com>
2021-05-03 16:18:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,13 +8,46 @@ const url2 = "mongodb+srv://annalisejacobson:annalise@cluster0.0y4mi.mongodb.net
 var app = express();
 app.use(express.static("public"));
 
-
+var file;
 app.get('/', function (req, res) {
-  var file = 'index.html';
+  file = 'index.html';
   fs.readFile(file, function(err, txt) {
       if(err) { return console.log(err); }
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(txt);
     });
 });
+app.get('/login.html', function (req, res) {
+  file = 'login.html';
+  fs.readFile(file, function(err, txt) {
+      if(err) { return console.log(err); }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(txt);
+    });
+});
+app.get('/my_choice.html', function (req, res) {
+  file = 'my_choice.html';
+  fs.readFile(file, function(err, txt) {
+      if(err) { return console.log(err); }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(txt);
+    });
+});
+app.get('/about.html', function (req, res) {
+  file = 'about.html';
+  fs.readFile(file, function(err, txt) {
+      if(err) { return console.log(err); }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(txt);
+    });
+});
+app.get('/menu.html', function (req, res) {
+  file = 'menu.html';
+  fs.readFile(file, function(err, txt) {
+      if(err) { return console.log(err); }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(txt);
+    });
+});
+setTimeout(function(){res.end();}, 2000);
 app.listen(port, function() {console.log("server started successfully");});

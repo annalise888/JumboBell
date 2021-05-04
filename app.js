c7d2314b10461f4645a3f22560386512f523cf5a
Calowsky <78949858+Calowsky@users.noreply.github.com>
2021-05-03 21:32:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -158,11 +158,11 @@ app.get('/my_choice.html/userprocess', function (req, res) {
 	//const queryObject = url.parse(req.url,true).query;
 	
   	res.write(req.url);
-	var myurl = req.url;
-	myurl = myurl.split("foodname=");
-	myurl = myurl[1];
+	//var myurl = req.url;
+	//myurl = myurl.split("foodname=");
+	//myurl = myurl[1];
   	//res.write(<br>);
-	res.write(myurl);
+	//res.write(myurl);
 
 
 	
